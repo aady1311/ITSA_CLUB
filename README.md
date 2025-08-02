54e10bd95280ee7de27f9788bff274f278cbde0c
Adity Yadav <aadityayadav7656@gmail.com>
2025-08-02 15:21:54 +0530

    feat(ui): enhance Home  section, clean unused code, and add new UI features

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ITSA_CLUB
+Done:-
+Siddhi / Aditya = Home, homepage animation , About , Team, Contact, etc
 
-Siddhi / Aditya = Home , About , Team, Contact, etc
+Pending  Section 
+siddhi => Logo , Tean section feacture 
+sai & Devyani =>   Events, achievement etc.
--- script.js
+++ script.js
@@ -279,5 +279,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
         
         showInitial();
+
+      
     });
 });
+
+
+let vantaEffect;
+
+function loadVanta(colorHex) {
+  if (vantaEffect) vantaEffect.destroy();
+  vantaEffect = VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: colorHex,
+    shininess: 80.00,
+    waveHeight: 25.00,
+    waveSpeed: 0.45,
+    zoom: 0.85
+  });
+}
+
+// on page load
+const html = document.documentElement;
+const savedTheme = localStorage.getItem("theme") || "light";
+html.setAttribute("data-theme", savedTheme);
+
+// check condition
+loadVanta(savedTheme === "dark" ? 0x0f172a : 0x2563eb);
+
+//  button click
+document.getElementById("themeToggle").addEventListener("click", () => {
+  const currentTheme = html.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  html.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+
+  // based on new theme
+  loadVanta(newTheme === "dark" ? 0x0f172a : 0x2563eb);
+});
+
+
+
+
+
